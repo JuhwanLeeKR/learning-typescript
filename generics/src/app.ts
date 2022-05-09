b68137a11a31bc6738a2461df5bef8a75a0e0066
Juhwan <dev.juhwanlee@gmail.com>
2022-05-10 01:46:43 +0900

    Add desc : generic 제약 조건

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,1 +1,44 @@
-// Code goes here!
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(()=>{
+//     resolve(10)
+//   },2000)
+// })
+
+// promise.then(data => {
+//   data.split(' ');
+// })
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergeObj = merge({ name: 'Juhwan', hobbies: ['Coding'] }, { age: 30 });
+console.log(mergeObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndPrint<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 0) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndPrint('Hi there!'));
+
+function extarctAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'Value: ' + obj[key];
+}
+
+extarctAndConvert({ name: 'Max' }, 'name');
