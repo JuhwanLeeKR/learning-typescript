cd78387ee06f4b3a9a5fd40586a9156b7d279965
Juhwan <dev.juhwanlee@gmail.com>
2022-05-09 21:15:34 +0900

    Add : class private, public 설명 및 약식 초기화 방식 설명

--- class-interface/src/app.ts
+++ class-interface/src/app.ts
@@ -1,1 +1,35 @@
-// Code goes here!
+class Department {
+  // private id: string;
+  // private name: string; // Obeject가 아닌 class의 field
+  private employees: string[] = []; // class 외부에서 접근을 막아준다.
+
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+  addEmployee(employee: string) {
+    // validation etc
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformaition() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department('d1', 'Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+accounting.describe();
+accounting.printEmployeeInformaition();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
