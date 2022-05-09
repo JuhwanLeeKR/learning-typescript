48795549435a995304899b7fa7b42ac5d4bc73d9
Juhwan <dev.juhwanlee@gmail.com>
2022-05-10 02:08:37 +0900

    Add desc : generic utility types

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -42,3 +42,25 @@ function extarctAndConvert<T extends object, U extends keyof T>(
 }
 
 extarctAndConvert({ name: 'Max' }, 'name');
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Juhwan', 'Max'];
+// names.push('Manu');
+// names.pop();
