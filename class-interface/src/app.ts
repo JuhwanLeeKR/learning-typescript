f44afbd27f6ffea344f87d2361863527bdcce8ed
Juhwan <dev.juhwanlee@gmail.com>
2022-05-09 22:46:57 +0900

    Add : Interface basic description

--- class-interface/src/app.ts
+++ class-interface/src/app.ts
@@ -1,67 +1,18 @@
-class Department {
-  // private readonly id: string;
-  // private name: string; // Obeject가 아닌 class의 field
-  private employees: string[] = []; // class 외부에서 접근을 막아준다.
+interface Person {
+  name: string;
+  age: number;
 
-  // private, public, readonly는 typescript에서만 가능하다.
-  constructor(private readonly id: string, public name: string) {
-    // this.id = id;
-    // this.name = n;
-  }
-
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
-  }
-  addEmployee(employee: string) {
-    // validation etc
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformaition() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+  greet(phrase: string): void;
 }
 
-class ITDepartment extends Department {
-  admins: string[];
-  constructor(id: string, admins: string[]) {
-    super(id, 'IT');
-    this.admins = admins;
-  }
-}
+let user1: Person;
 
-class AccountingDepartment extends Department {
-  constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting');
-  }
+user1 = {
+  name: 'Max',
+  age: 30,
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  },
+};
 
-  addReport(text: string) {
-    this.reports.push(text);
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-}
-
-const it = new ITDepartment('d1', ['Max']);
-
-it.addEmployee('Max');
-it.addEmployee('Manu');
-
-it.describe();
-it.name = 'NEW NAME';
-it.printEmployeeInformaition();
-
-console.log(it);
-
-const accounting = new AccountingDepartment('d2', []);
-
-accounting.addReport('Something ...');
-
-accounting.printReports();
-
-// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-
-// accountingCopy.describe();
+user1.greet('Hi there, I am');
