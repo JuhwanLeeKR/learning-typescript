03a3e609c2eb547b6c845648eea18489331661f2
Juhwan <dev.juhwanlee@gmail.com>
2022-05-09 23:22:36 +0900

    Add : interface desc

--- class-interface/src/app.ts
+++ class-interface/src/app.ts
@@ -1,5 +1,17 @@
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -7,21 +19,28 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Max');
+user1 = new Person();
+// user1.name = 'Manu';
 
-user1.greet('Hi there, I am');
+user1.greet('Hi there - I am');
 console.log(user1);
