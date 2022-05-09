4dba23682b3cae1ed21e80ee2a04fad5b429b31c
Juhwan <dev.juhwanlee@gmail.com>
2022-05-09 14:16:50 +0900

    Add : union types description

--- starting/app.js
+++ starting/app.js
@@ -1,44 +1,14 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: 'juhwan',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'auathor']
-// };
-// const ADMIN = 0;
-// const READ_ONY = 1;
-// const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'juhwan',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-// person.role.push('admin') // tuple에서 push를 걸러내지는 못한다.
-// person.role[1] = 10;
-// person.role = [0, 'admin', 'user']
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()) // !! ERR  !!
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
--- starting/app.ts
+++ starting/app.ts
@@ -1,52 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: 'juhwan',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'auathor']
-// };
-
-// const ADMIN = 0;
-// const READ_ONY = 1;
-// const AUTHOR = 2;
-
-enum Role {
-  ADMIN = 'ADMIN',
-  READ_ONLY = 100,
-  AUTHOR,
-}
-// 기본적으로 0,1,2로 시작되지만 특정 string이나 number로 지정할 수 있다.
-
-const person = {
-  name: 'juhwan',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN,
-};
-
-// person.role.push('admin') // tuple에서 push를 걸러내지는 못한다.
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user']
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()) // !! ERR  !!
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-if (person.role === Role.AUTHOR) {
-  console.log('is author');
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
