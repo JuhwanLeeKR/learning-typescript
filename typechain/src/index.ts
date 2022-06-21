d121e6beb4f2ee20cd1e9e769db12dfb44962816
Juhwan <dev.juhwanlee@gmail.com>
2022-06-21 19:36:31 +0900

    Add code: initial block

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,1 +1,23 @@
-console.log('bye');
+import crypto from 'crypto';
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash('sha256').update(toHash).digest('hex');
+  }
+}
